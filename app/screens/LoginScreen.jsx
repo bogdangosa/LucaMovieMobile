0baea7f2ad5bf2224afb0624efd93abfc04e55ea
denisdenis05 <denisgre205@gmail.com>
2024-03-31 12:52:27 +0300

    login screen logic

--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,40 +1,61 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet ,Text } from 'react-native';
+import InputField from '../../Components/FormElements/InputField';
+import SimpleButton from '../../Components/Buttons/SimpleButton';
+import { COLORS } from '../../Data/Colors';
+import axios from 'axios';
 
 const LoginScreen = ({navigation}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleLogin = () => {
-        // Implement your login logic here
+    const handleLogin = async() => {
+        try {
+            const response = await axios.post(process.env.SERVER_ADDRESS+'/api/login', {
+                email: email,
+                password: password
+            });
+            console.log("Login successful:", response.data);
+        } catch (error) {
+            console.error("Error occurred during login:", error);
+        }
+        console.log("response");
     };
 
     return (
-        <View style={styles.container}>
-            <TextInput
-                placeholder="Email"
-                value={email}
-                onChangeText={setEmail}
-            />
-            <TextInput
-                placeholder="Password"
-                secureTextEntry
-                value={password}
-                onChangeText={setPassword}
-            />
-            
-            <Button title="go to signup" onPress={()=>navigation.navigate("SignUp")} />
-            <Button title="Login" onPress={handleLogin} />
+        <View style={styles.login_container}>
+            <View style={styles.top_bar_container}>
+                <Text style={[styles.title, styles.accent]}>Login</Text>
+                <Text style={styles.title} onPress={()=>navigation.navigate("SignUp")}>Sign Up</Text>
+            </View>
+            <InputField label="Email" placeholder="Enter your email adress" value={email} onChangeText={setEmail}></InputField>
+            <InputField label="Password" placeholder="Enter your password" value={password} onChangeText={setPassword}></InputField>
+            <SimpleButton title="Sign Up" onPress={handleLogin} />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
+    top_bar_container: {
+        flexDirection: 'row',
+        gap: 16,
+    },
+    login_container: {
+        backgroundColor: COLORS.neutral,
+        height: '100%',
         flex: 1,
+        gap: 16,
         justifyContent: 'center',
         alignItems: 'center',
         padding: 16,
+    },
+    title: {
+        color: COLORS.primary,
+        fontSize: 24,
+        marginBottom: 16,
+    },
+    accent: {
+        color: COLORS.accent,
     },
 });
 
